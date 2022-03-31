a29ee5e8f284201b6225a4e1d3430aa251ce1b19
tkling <tyling@gmail.com>
2022-03-31 00:13:35 -0400

    checkpoint: 42%

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,21 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    unique_sides = {a, b, c}
+
+    if len(list(filter(lambda x: (x <= 0), unique_sides))) > 0:
+        raise TriangleError("LOL!")
+
+    if (a + b <= c) or (a + c <= b) or (b + c <= a):
+        raise TriangleError("LOL!")
+
+    triangle_types = {
+        1: "equilateral",
+        2: "isosceles",
+        3: "scalene"
+    }
+
+    return triangle_types.get(len(unique_sides))
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
